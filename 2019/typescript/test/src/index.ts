b6881320367aeeec598ce8fdcaab2ccf4316ab53
jh1016248 <1774643394@qq.com>
2019-06-12 16:11:28 +0800

    update

---
+++ 2019/typescript/test/src/index.ts
@@ -0,0 +1,8 @@
+let sArr: number[];
+console.log(sArr)
+const a = '123'
+log(a)
+
+function log(str: string|number):void {
+    console.log(str)
+}
---
+++ 2019/typescript/test/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //打包html的插件
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader",
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./index.html'  
+        })
+    ]
+};
